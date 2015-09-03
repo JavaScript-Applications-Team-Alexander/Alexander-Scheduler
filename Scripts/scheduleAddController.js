165f94fa0a0cb01fc96f8f48d0d9ca1484e176b8
mr.krustev <mr.krustev1994@gmail.com>
2015-09-04 02:07:28 +0300

    Finished editting the unit test demo.

--- Scripts/scheduleAddController.js
+++ Scripts/scheduleAddController.js
@@ -1,147 +1,119 @@
-// Define jQuery and scheduleAddController(as DOMElements)
-var scheduleAddController = (function () {
-    var scheduleAddController = Object.create({});
-    var HEIGTH_OF_ELEMENT = 100;
+import DOMElements from 'Scripts/scheduleAddControllerDOMElements.js';
 
-    Object.defineProperty(scheduleAddController, 'init', {
-        value: function (selector) {
-            //TODO: Validate
-            $('<div/>', {
-                id: 'schedule-add-controller'
-            }).appendTo(selector);
-            visualize();
-            showInitialState();
-        }
-    });
+var HEIGTH_OF_ELEMENT = 100;
 
-    Object.defineProperty(scheduleAddController, 'dispose', {
-        value: function () {
-            var $controller = $('#schedule-add-controller');
-            hide();
-            $controller.parent().get(0).removeChild($controller.get(0));
-        }
-    });
+function init(selector){
+    var $element = $(selector);
+    if($element.get(0) == undefined){
+        throw new Error('Element with given selector could not be found.');
+    }
+    $('<div/>').addClass('scheduleAddController').appendTo($element);
+    visualize();
+    showInitialState();
+}
 
+function dispose(){
+    var $controller = $('.scheduleAddController');
+    if($controller.get(0) == undefined){
+        throw new Error('Controller must be initiated first.');
+    }
+    hide();
+    $controller.parent().get(0).removeChild($controller.get(0));
+}
 
-    // TODO: Rethink the visualize/hide part
-    function visualize() {
-        var controllerDOMElement = $('#schedule-add-controller');
-        var i = 0,
-            len = HEIGTH_OF_ELEMENT;
-        controllerDOMElement.show();
-        requestAnimationFrame(show);
-        function show() {
-            i += 5;
-            controllerDOMElement.css('height', i + 'px');
-            if (i < len) {
-                requestAnimationFrame(show);
-            }
+// TODO: Rethink the visualize/hide part
+function visualize() {
+    var controllerDOMElement = $('.scheduleAddController');
+    var i = 0,
+        len = HEIGTH_OF_ELEMENT;
+    controllerDOMElement.show();
+    requestAnimationFrame(show);
+    function show() {
+        i += 5;
+        controllerDOMElement.css('height', i + 'px');
+        if (i < len) {
+            requestAnimationFrame(show);
         }
     }
+}
+
+function hide() {
+    var controllerDOMElement = $('.scheduleAddController');
+    var i = HEIGTH_OF_ELEMENT,
+        len = 0;
 
     function hide() {
-        var controllerDOMElement = $('#schedule-add-controller');
-        var i = HEIGTH_OF_ELEMENT,
-            len = 0;
-
-        function hide() {
-            i -= 5;
-            controllerDOMElement.css('height', i + 'px');
-            if (i > len) {
-                requestAnimationFrame(hide);
-            } else {
-                controllerDOMElement.hide();
-            }
-        }
-
-        requestAnimationFrame(hide);
-    }
-
-    function renderUnusualEventAdding() {
-        var result = document.createDocumentFragment();
-        result.appendChild(DOMElements.getBackButton());
-        result.appendChild(DOMElements.getNameInput());
-        result.appendChild(DOMElements.getStartHourInput());
-        result.appendChild(DOMElements.getEndHourInput());
-        result.appendChild(DOMElements.getDescriptionInput());
-        result.appendChild(DOMElements.getAddButton());
-        result.appendChild(DOMElements.getCloseButton());
-        return result;
-    }
-
-    function renderUsualEventAdding() {
-        var result = document.createDocumentFragment();
-        result.appendChild(DOMElements.getBackButton());
-        result.appendChild(DOMElements.getAppointmentListInput());
-        result.appendChild(DOMElements.getDurationInput());
-        result.appendChild(DOMElements.getAddButton());
-        result.appendChild(DOMElements.getCloseButton());
-        return result;
-    }
-
-    function renderInitialState() {
-        var result = document.createDocumentFragment();
-        result.appendChild(DOMElements.getUsualButton());
-        result.appendChild(DOMElements.getUnusualButton());
-        result.appendChild(DOMElements.getCloseButton());
-        return result;
-    }
-
-    function saveApppointment(ev){
-        // NOT TESTED
-        if(ev.target.id === 'controller-usual-event-button'){
-            var appointmentValue = $('#controller-appointment-input').get(0).value;
-            var durationValue = $('#controller-duration-input').get(0).value;
-
-
-        } else if (ev.target.id === 'controller-unusual-event-button'){
-            var nameValue = $('#controller-appointment-name-input').get(0).value;
-            var startHourValue = $('#controller-appointment-start-hour-input').get(0).value;
-            var endHourValue = $('#controller-appointment-end-hour-input').get(0).value;
-            var descriptionValue = $('#controller-appointment-description-input').get(0).value;
-
+        i -= 5;
+        controllerDOMElement.css('height', i + 'px');
+        if (i > len) {
+            requestAnimationFrame(hide);
         } else {
-            throw new Error('Cannot add event.')
+            controllerDOMElement.hide();
         }
-        //TODO Make logic with database.
     }
 
-    function showUnusualEventAdding() {
-        var $output = $('#schedule-add-controller');
-        var result = renderUnusualEventAdding();
+    requestAnimationFrame(hide);
+}
 
-        $output.empty();
-        $output.append(result);
-        $('#controller-back-button').on('click', showInitialState);
-        $('#controller-close-button').on('click', dispose);
-        $('#controller-add-button').on('click', saveApppointment);
-    }
+function renderUnusualEventAdding() {
+    var result = document.createDocumentFragment();
+    result.appendChild(DOMElements.getBackButton());
+    result.appendChild(DOMElements.getNameInput());
+    result.appendChild(DOMElements.getStartHourInput());
+    result.appendChild(DOMElements.getEndHourInput());
+    result.appendChild(DOMElements.getDescriptionInput());
+    result.appendChild(DOMElements.getAddButton());
+    result.appendChild(DOMElements.getCloseButton());
+    return result;
+}
 
-    function showUsualEventAdding() {
-        var $output = $('#schedule-add-controller');
-        var result = renderUsualEventAdding();
+function renderUsualEventAdding() {
+    var result = document.createDocumentFragment();
+    result.appendChild(DOMElements.getBackButton());
+    result.appendChild(DOMElements.getAppointmentListInput());
+    result.appendChild(DOMElements.getDurationInput());
+    result.appendChild(DOMElements.getAddButton());
+    result.appendChild(DOMElements.getCloseButton());
+    return result;
+}
 
-        $output.empty();
-        $output.append(result);
-        $('#controller-back-button').on('click', showInitialState);
-        $('#controller-close-button').on('click', dispose);
-        $('#controller-add-button').on('click', saveApppointment);
-    }
+function renderInitialState() {
+    var result = document.createDocumentFragment();
+    result.appendChild(DOMElements.getUsualButton());
+    result.appendChild(DOMElements.getUnusualButton());
+    result.appendChild(DOMElements.getCloseButton());
+    return result;
+}
 
-    function showInitialState() {
-        var $output = $('#schedule-add-controller');
-        var result = renderInitialState();
+function showUnusualEventAdding() {
+    var $output = $('.scheduleAddController');
+    var result = renderUnusualEventAdding();
 
-        $output.empty();
-        $output.append(result);
-        $('#controller-usual-event-button').on('click', showUsualEventAdding);
-        $('#controller-unusual-event-button').on('click', showUnusualEventAdding);
-        $('#controller-close-button').on('click', dispose);
-    }
+    $output.empty();
+    $output.append(result);
+    $('#controller-back-button').on('click', showInitialState);
+    $('#controller-close-button').on('click', dispose);
+}
 
-    return scheduleAddController;
-}());
+function showUsualEventAdding() {
+    var $output = $('.scheduleAddController');
+    var result = renderUsualEventAdding();
 
+    $output.empty();
+    $output.append(result);
+    $('#controller-back-button').on('click', showInitialState);
+    $('#controller-close-button').on('click', dispose);
+}
 
+function showInitialState() {
+    var $output = $('.scheduleAddController');
+    var result = renderInitialState();
 
+    $output.empty();
+    $output.append(result);
+    $('#controller-usual-event-button').on('click', showUsualEventAdding);
+    $('#controller-unusual-event-button').on('click', showUnusualEventAdding);
+    $('#controller-close-button').on('click', dispose);
+}
 
+export default {init,dispose,visualize,hide}
--- Scripts/scheduleAddControllerDOMElements.js
+++ Scripts/scheduleAddControllerDOMElements.js
@@ -1,210 +1,190 @@
-// Import/define jquery
+import 'jquery';
 
-var scheduleAddControllerDOMElements = (function () {
-    var scheduleAddControllerDOMElements = Object.create({});
-
-    Object.defineProperty(scheduleAddControllerDOMElements, 'getUsualButton', {
-        value: function () {
-            var usualEventBox = $('<div/>', {
-                id: 'controller-usual-event-button-box'
-            });
-            var usualEventButton = $('<button/>', {
-                id: 'controller-usual-event-button'
-            })
-                .addClass('btn')
-                .html('Add Usual Event');
-            usualEventBox.append(usualEventButton);
-            //Makes the jQuery object into a normal js DOM element.
-            usualEventBox = usualEventBox.get(0);
-            return usualEventBox;
-        }
+function getUsualButton() {
+    var usualEventBox = $('<div/>', {
+        id: 'controller-usual-event-button-box'
     });
+    var usualEventButton = $('<button/>', {
+        id: 'controller-usual-event-button'
+    })
+        .addClass('btn')
+        .html('Add Usual Event');
+    usualEventBox.append(usualEventButton);
+    //Makes the jQuery object into a normal js DOM element.
+    usualEventBox = usualEventBox.get(0);
+    return usualEventBox;
+}
 
-    Object.defineProperty(scheduleAddControllerDOMElements, 'getUnusualButton', {
-        value: function () {
-            var unusualEventBox = $('<div/>', {
-                id: 'controller-unusual-event-button-box'
-            });
-            var unusualEventButton = $('<button/>', {
-                id: 'controller-unusual-event-button'
-            })
-                .addClass('btn')
-                .html('Add Unusual Event');
-            unusualEventBox.append(unusualEventButton);
-            unusualEventBox = unusualEventBox.get(0);
-            return unusualEventBox;
-        }
+function getUnusualButton() {
+    var unusualEventBox = $('<div/>', {
+        id: 'controller-unusual-event-button-box'
     });
+    var unusualEventButton = $('<button/>', {
+        id: 'controller-unusual-event-button'
+    })
+        .addClass('btn')
+        .html('Add Unusual Event');
+    unusualEventBox.append(unusualEventButton);
+    unusualEventBox = unusualEventBox.get(0);
+    return unusualEventBox;
+}
 
-    Object.defineProperty(scheduleAddControllerDOMElements, 'getBackButton', {
-        value: function () {
-            var backButtonBox = $('<div/>', {
-                id: 'controller-back-button-box'
-            });
-            var backButton = $('<button/>', {
-                id: 'controller-back-button'
-            })
-                .addClass('btn')
-                .html('Back');
-            backButtonBox.append(backButton);
-            backButtonBox = backButtonBox.get(0);
-            return backButtonBox;
-        }
+function getBackButton() {
+    var backButtonBox = $('<div/>', {
+        id: 'controller-back-button-box'
     });
+    var backButton = $('<button/>', {
+        id: 'controller-back-button'
+    })
+        .addClass('btn')
+        .html('Back');
+    backButtonBox.append(backButton);
+    backButtonBox = backButtonBox.get(0);
+    return backButtonBox;
+}
 
-    Object.defineProperty(scheduleAddControllerDOMElements, 'getAppointmentListInput', {
-        value: function () {
-            var appointmentBox = $('<div/>', {
-                id: 'controller-appointment-box'
-            });
-            var appointmentLabel = $('<label/>', {
-                for: 'controller-appointment-input'
-            })
-                .html('Appointment');
-            appointmentBox.append(appointmentLabel);
-            var appointmentDropdown = $('<select/>', {
-                id: 'controller-appointment-input'
-            });
-            // TODO: Make connection to Database
-            var testOption = $('<option/>', {
-                value: 'haircut'
-            })
-                .html('Haircut');
-            appointmentDropdown.append(testOption);
-            appointmentBox.append(appointmentDropdown);
-            appointmentBox = appointmentBox.get(0);
-            return appointmentBox;
-        }
+function getAppointmentListInput() {
+    var appointmentBox = $('<div/>', {
+        id: 'controller-appointment-box'
     });
-    Object.defineProperty(scheduleAddControllerDOMElements, 'getDurationInput', {
-        value: function () {
-            var durationBox = $('<div/>', {
-                id: 'controller-duration-box'
-            });
-            var durationLabel = $('<label/>', {
-                for: 'controller-duration-input'
-            })
-                .html('Duration');
-            durationBox.append(durationLabel);
-            var durationInput = $('<input/>', {
-                type: 'time',
-                id: 'controller-duration-input'
-            });
-            durationBox.append(durationInput);
-            durationBox = durationBox.get(0);
-            return durationBox;
-        }
+    var appointmentLabel = $('<label/>', {
+        for: 'controller-appointment-dropdown-list'
+    })
+        .html('Appointment');
+    appointmentBox.append(appointmentLabel);
+    var appointmentDropdown = $('<select/>', {
+        id: 'controller-appointment-dropdown-list'
     });
+    // TODO: Make connection to Database
+    var testOption = $('<option/>', {
+        value: 'haircut'
+    })
+        .html('Haircut');
+    appointmentDropdown.append(testOption);
+    appointmentBox.append(appointmentDropdown);
+    appointmentBox = appointmentBox.get(0);
+    return appointmentBox;
+}
 
-    Object.defineProperty(scheduleAddControllerDOMElements, 'getNameInput', {
-        value: function () {
-            var nameBox = $('<div/>', {
-                id: 'controller-name-box'
-            });
-            var nameLabel = $('<label/>', {
-                for: 'controller-appointment-name-input'
-            })
-                .html('Name:');
-            nameBox.append(nameLabel);
-            var nameInput = $('<input/>', {
-                type: 'text',
-                id: 'controller-appointment-name-input',
-                placeholder: 'Appointment name'
-            });
-            nameBox.append(nameInput);
-            nameBox = nameBox.get(0);
-            return nameBox;
-        }
+function getDurationInput() {
+    var durationBox = $('<div/>', {
+        id: 'controller-duration-box'
     });
-
-
-    Object.defineProperty(scheduleAddControllerDOMElements, 'getStartHourInput', {
-        value: function () {
-            var startHourBox = $('<div/>', {
-                id: 'controller-start-hour-box'
-            });
-            var startHourLabel = $('<label/>', {
-                for: 'controller-appointment-start-hour-input'
-            })
-                .html('From');
-            startHourBox.append(startHourLabel);
-            var startHourInput = $('<input/>', {
-                type: 'time',
-                id: 'controller-appointment-start-hour-input'
-            });
-            startHourBox.append(startHourInput);
-            startHourBox = startHourBox.get(0);
-            return startHourBox;
-        }
+    var durationLabel = $('<label/>', {
+        for: 'controller-duration-input'
+    })
+        .html('Duration');
+    durationBox.append(durationLabel);
+    var durationInput = $('<input/>', {
+        type: 'time',
+        id: 'controller-duration-input'
     });
+    durationBox.append(durationInput);
+    durationBox = durationBox.get(0);
+    return durationBox;
+}
 
-
-    Object.defineProperty(scheduleAddControllerDOMElements, 'getEndHourInput', {
-        value: function () {
-            var endHourBox = $('<div/>', {
-                id: 'controller-end-hour-box'
-            });
-            var endHourLabel = $('<label/>', {
-                for: 'controller-appointment-end-hour-input'
-            })
-                .html('To');
-            endHourBox.append(endHourLabel);
-            var endHourInput = $('<input/>', {
-                type: 'time',
-                id: 'controller-appointment-end-hour-input'
-            });
-            endHourBox.append(endHourInput);
-            endHourBox = endHourBox.get(0);
-            return endHourBox;
-        }
+function getNameInput() {
+    var nameBox = $('<div/>', {
+        id: 'controller-name-box'
     });
-
-    Object.defineProperty(scheduleAddControllerDOMElements, 'getDescriptionInput', {
-        value: function () {
-            var descriptionBox = $('<div/>', {
-                id: 'controller-description-box'
-            });
-            var descriptionLabel = $('<label/>', {
-                for: 'controller-appointment-description-input'
-            })
-                .html('Description');
-            descriptionBox.append(descriptionLabel);
-            var descriptionInput = $('<textarea/>', {
-                id: 'controller-appointment-end-hour-input',
-                placeholder: 'Extra info.'
-            });
-            descriptionBox.append(descriptionInput);
-            descriptionBox = descriptionBox.get(0);
-            return descriptionBox;
-        }
+    var nameLabel = $('<label/>', {
+        for: 'controller-appointment-name-input'
+    })
+        .html('Name:');
+    nameBox.append(nameLabel);
+    var nameInput = $('<input/>', {
+        type: 'text',
+        id: 'controller-appointment-name-input',
+        placeholder: 'Appointment name'
     });
+    nameBox.append(nameInput);
+    nameBox = nameBox.get(0);
+    return nameBox;
+}
 
-
-    Object.defineProperty(scheduleAddControllerDOMElements, 'getAddButton', {
-        value: function () {
-            var addAppointmentButton = $('<div/>',{
-                id: 'controller-add-button'
-            })
-                .addClass('btn')
-                .html('Add Appointment');
-            addAppointmentButton = addAppointmentButton.get(0);
-            return addAppointmentButton;
-        }
+function getStartHourInput() {
+    var startHourBox = $('<div/>', {
+        id: 'controller-start-hour-box'
     });
-
-
-    Object.defineProperty(scheduleAddControllerDOMElements, 'getCloseButton', {
-        value: function () {
-            var closeButton = $('<div/>', {
-                id: 'controller-close-button'
-            })
-                .addClass('btn')
-                .html('x')
-                .get(0);
-            return closeButton;
-        }
+    var startHourLabel = $('<label/>', {
+        for: 'controller-appointment-start-hour-input'
+    })
+        .html('From');
+    startHourBox.append(startHourLabel);
+    var startHourInput = $('<input/>', {
+        type: 'time',
+        id: 'controller-appointment-start-hour-input'
     });
+    startHourBox.append(startHourInput);
+    startHourBox = startHourBox.get(0);
+    return startHourBox;
+}
 
+function getEndHourInput() {
+    var endHourBox = $('<div/>', {
+        id: 'controller-end-hour-box'
+    });
+    var endHourLabel = $('<label/>', {
+        for: 'controller-appointment-end-hour-input'
+    })
+        .html('To');
+    endHourBox.append(endHourLabel);
+    var endHourInput = $('<input/>', {
+        type: 'time',
+        id: 'controller-appointment-end-hour-input'
+    });
+    endHourBox.append(endHourInput);
+    endHourBox = endHourBox.get(0);
+    return endHourBox;
+}
 
-    return scheduleAddControllerDOMElements;
-})();
+function getDescriptionInput() {
+    var descriptionBox = $('<div/>', {
+        id: 'controller-description-box'
+    });
+    var descriptionLabel = $('<label/>', {
+        for: 'controller-appointment-description-input'
+    })
+        .html('Description');
+    descriptionBox.append(descriptionLabel);
+    var descriptionInput = $('<textarea/>', {
+        id: 'controller-appointment-end-hour-input',
+        placeholder: 'Extra info.'
+    });
+    descriptionBox.append(descriptionInput);
+    descriptionBox = descriptionBox.get(0);
+    return descriptionBox;
+}
+
+function getAddButton() {
+    var addAppointmentButton = $('<div/>')
+        .addClass('btn')
+        .html('Add Appointment');
+    addAppointmentButton = addAppointmentButton.get(0);
+    return addAppointmentButton;
+}
+
+function getCloseButton() {
+    var closeButton = $('<div/>', {
+        id: 'controller-close-button'
+    })
+        .addClass('btn')
+        .html('x')
+        .get(0);
+    return closeButton;
+}
+
+export default {
+    getUsualButton,
+    getUnusualButton,
+    getBackButton,
+    getAppointmentListInput,
+    getDurationInput,
+    getNameInput,
+    getStartHourInput,
+    getEndHourInput,
+    getDescriptionInput,
+    getAddButton,
+    getCloseButton
+}
